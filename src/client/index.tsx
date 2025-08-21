074357e7bdd05b4ba987d7e70754aeb578539e5b
Kevin Yamada <kyoseix@gmail.com>
2025-08-21 15:59:43 -0700

    added analytics, added ability to filter out not-rated movies from comparison page

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/next";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
     <Analytics />
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
